fb799a8bdf53d6801d7b720c6b1dffccff2e7768
oshirok <oshiro@uw.edu>
2014-10-18 09:34:30 -0700

    facebookfix

--- facebookAuth/auth.js
+++ facebookAuth/auth.js
@@ -2,6 +2,6 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' : '601766443284171',
 		'clientSecret' : '2cb47c9b5d5e9d755da433e774d64db3',
-		'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
+		'callbackURL' : 'http://bitpuzzlegame.herokuapp.com/auth/facebook/callback'
 	}
 };
