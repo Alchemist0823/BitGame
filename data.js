4436629241f81e02f222e06ec4b1ac7b8f43f91d
noname01 <hjc.noname@gmail.com>
2014-10-18 02:01:09 -0700

    judging api

--- data.js
+++ data.js
@@ -1,39 +1,20 @@
-var baseIntProgram =
-    '#include<stdio.h>\n' +
-    'int x,y,z,res;' +
-    'int main(){' +
-        'scanf("%d%d%d",&x,&y,&z);' +
-        'res = userinput;' +
-        'printf("%d",res);' +
-        'return 0;' +
-    '}';
-
-
 var probs = {
     "0" : {
         "title": "level 1",
         "description": "task 1",
-        "program": baseIntProgram
-    },
-    "1" : {
-        "title": "level 2",
-        "description": "task 11",
-        "program": ""
-    },
-    "2" : {
-        "title": "level 3",
-        "description": "task 111",
-        "program": ""
-    },
-    "3" : {
-        "title": "level 4",
-        "description": "task 121",
-        "program": ""
-    },
-    "4" : {
-        "title": "level 5",
-        "description": "task 123",
-        "program": ""
+        "operators": {
+            "&": 1,
+            "|": 1,
+            "&&": 1,
+            "||": 1,
+            "<<": 1,
+            ">>": 1,
+            "^": 1,
+            "!": 1,
+            "~": 1
+        },
+        "input": ["1", "2", "3"],
+        "output": ["2", "4", "6"]
     }
 };
 
