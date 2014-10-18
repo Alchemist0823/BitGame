dd17d35a85fcbfc41f6557db869ae6fa65f3a98e
Alchemist0823 <internetboy0823@gmail.com>
2014-10-18 10:40:02 -0700

    add problems

--- data.js
+++ data.js
@@ -38,6 +38,40 @@ var probs = {
         "input": ["2", "4", "8"],
         "output": ["1", "2", "4"],
         "level": [1, 2]
+    },
+    "4" : {
+        "title": "level 5",
+        "description": "isPower2 - returns 1 if x is a power of 2, and 0 otherwise.  Examples: isPower2(5) = 0, isPower2(8) = 1, isPower2(0) = 0. Note that no negative number is a power of 2.",
+        "operators": {
+            "!": 5,
+            "|": 5,
+            "&": 5,
+            "<<": 5,
+            ">>": 5,
+            "^": 5,
+            "+": 5,
+            "~": 5
+        },
+        "input": ["4", "5", "8","-4","1"],
+        "output": ["1", "0", "1","0","1"],
+        "level": [10, 15]
+    },
+    "5" : {
+        "title": "level 6",
+        "description": " conditional - same as x ? y : z. Example: conditional(2,4,5) = 4. ",
+        "operators": {
+            "!": 5,
+            "|": 5,
+            "&": 5,
+            "<<": 5,
+            ">>": 5,
+            "^": 5,
+            "+": 5,
+            "~": 5
+        },
+        "input": ["10 2 1", "1 2 1", "0 1 100"],
+        "output": ["2", "2", "100"],
+        "level": [10, 15]
     }
 };
 
@@ -61,6 +95,10 @@ var list = {
     "4" : {
         "pid": "4",
         "title": "level 5"
+    },
+    "5" : {
+        "pid": "5",
+        "title": "level 6"
     }
 };
 
